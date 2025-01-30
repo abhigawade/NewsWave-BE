8ff73abb9cc21b09cfefdc874cc79593098d8b51
Abhishek-Gawade-1 <abhishek@metamultiples.in>
2025-01-30 11:28:28 +0530

    core header added to connect with FE

--- NewsWave/NewsWave/settings.py
+++ NewsWave/NewsWave/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     
+    'corsheaders',
+    
     'rest_framework',
     'rest_framework_simplejwt',
     'rest_framework_simplejwt.token_blacklist',
@@ -52,6 +54,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -68,6 +71,12 @@ REST_FRAMEWORK = {
     ),
     
 }
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",  # Frontend's origin
+]
+
+CORS_ALLOW_ALL_ORIGINS = True #change it when project completed
 
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(days=30),
