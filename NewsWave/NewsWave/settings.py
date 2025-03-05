3f74210d3a87931e17fd56366dac33a9cbc1d847
Abhishek-Gawade-1 <abhishek@metamultiples.in>
2025-03-05 16:29:47 +0530

    changes settings

--- NewsWave/NewsWave/settings.py
+++ NewsWave/NewsWave/settings.py
@@ -243,8 +243,8 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "abhishekgawade2304@gmail.com"  
-EMAIL_HOST_PASSWORD = "jwveaykahgnrnslo"  # Use an App Password (not your Gmail password)
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER") 
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 
 
 
