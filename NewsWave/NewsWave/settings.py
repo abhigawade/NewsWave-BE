967040adb9ad4704a14044a73795028d9026e6e6
Abhishek-Gawade-1 <abhishek@metamultiples.in>
2025-03-08 11:59:46 +0530

    static file changes

--- NewsWave/NewsWave/settings.py
+++ NewsWave/NewsWave/settings.py
@@ -238,16 +238,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
-
-if os.environ.get('RENDER'):
-    STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
